5181c01ece238e377a60c43381d2c6177c67a923
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-04-13 22:05:02 +0800

    4.13 Submission2

---
+++ Array/src/Maximum_Subarray_53.java
@@ -0,0 +1,15 @@
+public class Maximum_Subarray_53 {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = nums[0];
+        for (int i=1; i < nums.length; i++){
+            sum = nums[i] + ((sum>0) ? sum: 0);
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+
+    public int maxSubArray2(int[] A){
+        
+    }
+}
