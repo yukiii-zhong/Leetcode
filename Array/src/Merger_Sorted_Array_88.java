0e981b1a60b4a7554806b371a622180daa39d9e6
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-04-15 14:09:59 +0800

    88. Submit

---
+++ Array/src/Merger_Sorted_Array_88.java
@@ -0,0 +1,19 @@
+public class Merger_Sorted_Array_88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        while (m>=1 && n>=1){
+            if (nums1[m-1] > nums2[n-1]){
+                nums1[m+n-1] = nums1[m-1];
+                m--;
+            }
+            else{
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
+        }
+        if (n>0){
+            for (int i =0; i<n;i++){
+                nums1[i] = nums2[i];
+            }
+        }
+    }
+}
