712e26d8baf6de53c0159c026e82f3ca1bdffc36
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-04-15 10:46:31 +0800

    122 Submit

--- Array/src/Best_Time_to_Buy_and_Sell_Stock_II.java
+++ Array/src/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -1,3 +1,9 @@
 public class Best_Time_to_Buy_and_Sell_Stock_II {
-    
+    public static int maxProfit(int[] prices){
+        int profit = 0;
+        for (int i=0; i<prices.length-1; i++){
+            profit += Math.max(prices[i+1]-prices[i], 0);
+        }
+        return profit;
+    }
 }
