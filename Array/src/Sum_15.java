3762e70b2299ad06d7b7c6d45686fea6c94f28ea
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-04-21 16:46:30 +0800

    01 method2

--- Array/src/Sum_15.java
+++ Array/src/Sum_15.java
@@ -6,6 +6,6 @@ public class Sum_15 {
     public List<List<Integer>> threeSum(int[] Sums){
         Arrays.sort(Sums);
         List<List<Integer>> ans = new ArrayList<>();
-        
+
     }
 }
--- Array/src/Two_Sum_01.java
+++ Array/src/Two_Sum_01.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Two_Sum_01 {
     public int[] twoSum(int[] nums, int target) {
         for (int i=0; i<nums.length;i++){
@@ -10,5 +13,20 @@ public class Two_Sum_01 {
             }
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSum2(int[] nums, int target){
+        Map<Integer, Integer> seen = new HashMap<>();
+        int[] ans = new int[2];
+        for (int i = 0; i<nums.length;i++){
+            if (seen.containsKey(target - nums[i])){
+                ans[1] = i;
+                ans[0] = seen.get(target-nums[i]);
+            }
+            else{
+                seen.put(nums[i],i);
+            }
+        }
+        return ans;
     }
 }
