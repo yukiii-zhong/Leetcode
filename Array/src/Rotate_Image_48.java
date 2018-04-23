10a5ea0876a4c19ec2d550622aa33893833a5463
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-04-23 15:28:18 +0800

    48 submmit

--- Array/src/Rotate_Image_48.java
+++ Array/src/Rotate_Image_48.java
@@ -1,5 +1,23 @@
 public class Rotate_Image_48 {
     public void rotate(int[][] matrix) {
-        
+        int n = matrix.length;
+        int t = matrix.length-1;
+        for (int i=0;i < (n-1)/2+1;i++){
+            for (int j=i;j<n-i-1;j++){
+                int temp = matrix[i][j];
+                int a =i;
+                int b = j;
+                for (int k=0; k<4;k++){
+                    int temp2 = matrix[b][t-a];
+                    matrix[b][t-a] = temp;
+                    temp = temp2;
+
+                    int z = a;
+                    a = b;
+                    b = t-z;
+                }
+            }
+
+        }
     }
 }
