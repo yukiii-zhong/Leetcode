db8689829876b02c70a00a76e44df62906f3f689
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-04-29 16:17:16 +0800

    136 method2 submmit

--- Array/src/136. Single Number.py
+++ Array/src/136. Single Number.py
@@ -10,4 +10,17 @@ class Solution:
             if nums[i] != nums[i + 1]:
                 return nums[i]
 
-        return nums[len(nums) - 1]
+        return nums[-1]
+
+    def singleNumber2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        a = 0
+
+        for i in nums:
+            a ^= i
+
+        return a
