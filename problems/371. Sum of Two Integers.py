62cee31a0bd7fd11389954e90403444765e36713
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-02 01:36:40 +0800

    461 submmit

--- problems/371. Sum of Two Integers.py
+++ problems/371. Sum of Two Integers.py
@@ -20,14 +20,19 @@ class Solution:
         :type b: int
         :rtype: int
         """
+        # 32 bits integer max
         MAX = 0x7FFFFFFF
+        # 32 bits interger min
         MIN = 0x80000000
+        # mask to get last 32 bits
         mask = 0xFFFFFFFF
-
-        while b !=0:
-            a, b = (a^b) & mask, ((a&b)<<1) & mask
-
+        while b != 0:
+            # ^ get different bits and & gets double 1s, << moves carry
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
         return a if a <= MAX else ~(a ^ mask)
 
-test1 = Solution().getSum(2,-3)
-print(test1)
+s = bin(2^5)
+i = int("111",2)
+print(i)
