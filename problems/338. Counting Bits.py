fc21e408dad595d3b58946c9043c720ec449699b
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-02 20:06:26 +0800

    338 submmit

---
+++ problems/338. Counting Bits.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(num+1):
+            ans.append(bin(i).count("1"))
+        return ans
+
+
+    # 
+    def countBits2(self, num):
+
+        ans = [0]
+        if num == 0: return ans
+
+        for i in range(1,num+1):
+            ans.append(ans[i & (i-1)] +1)
+
+        return ans
+
+test1 = Solution().countBits2(0)
+print(test1)
