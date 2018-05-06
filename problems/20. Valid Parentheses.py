a7ab6a2c7c7d3b07e059b54b9b973cac5d6a0b53
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-06 16:46:22 +0800

    20

--- problems/20. Valid Parentheses.py
+++ problems/20. Valid Parentheses.py
@@ -4,3 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
+        dic = {'(':-1, '[':-2, '{':-3, ')':1, ']':2, '}':3}
+
+        temp = []
+
+        for para in s:
+
+            if dic[para] < 0:
+                temp.append(para)
+            elif dic[para] > 0 and len(temp) and dic[temp[-1]] + dic[para] == 0:
+                temp.pop()
+            else:
+                return False
+
+        if len(temp) == 0:
+            return True
+        else:
+            return False
+
+test1 = ')'
+test2 = '[(){}]'
+test3 = '[(])'
+print(Solution().isValid(test1))
