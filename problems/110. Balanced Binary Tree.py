72f1d26253d823608ad58429ef5ed4ad58d331f4
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-07 19:56:06 +0800

    110 method2

--- problems/110. Balanced Binary Tree.py
+++ problems/110. Balanced Binary Tree.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Solution:
-    def isBalanced(self, root):
+    def isBalanced2(self, root):
         """
         :type root: TreeNode
         :rtype: bool
@@ -27,3 +27,24 @@ class Solution:
             return self.isBalanced(root.left) and self.isBalanced(root.right)
         else:
             return False
+
+    # Improvement based on previous solution
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        self.res = True
+
+
+        def dfs(root):
+            if root is None: return 0
+
+            p = dfs(root.left)
+            q = dfs(root.right)
+            if abs(p-q) > 1: self.res = False
+
+            return 1 + max(p,q)
+
+        dfs(root)
+        return self.res
