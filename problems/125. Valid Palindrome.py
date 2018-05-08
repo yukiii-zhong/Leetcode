b64345c0753878f32c76b513af95a808f57093ab
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-08 21:34:05 +0800

    125

---
+++ problems/125. Valid Palindrome.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) == 0: return True
+
+        i, j = 0, len(s)-1
+
+        while i<j:
+            if not s[i].isalnum():
+                i +=1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+
+            if s[i].lower() != s[j].lower():
+                return False
+
+            i +=1
+            j -=1
+
+        return True
+
+print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
