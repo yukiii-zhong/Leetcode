c0c8874c6647c95b90e49edff43d26a90d05a993
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-09 15:58:47 +0800

    345

--- problems/345. Reverse Vowels of a String.py
+++ problems/345. Reverse Vowels of a String.py
@@ -19,4 +19,28 @@ class Solution:
 
         return "".join(sList)
 
-print(Solution().reverseVowels('leetcode'))
+
+    def reverseVowels2(self, s):
+        vow = 'aeiouAEIOU'
+
+        if len(s) <= 1: return s
+
+        i = 0
+        j = len(s)-1
+        sList = list(s)
+
+        while j>i:
+            while j>i and sList[i] not in vow:
+                i+=1
+            while j>i and sList[j] not in vow:
+                j-=1
+            if i<j:
+                sList[i],sList[j] = sList[j],sList[i]
+                i +=1
+                j -=1
+
+        return "".join(sList)
+
+
+
+print(Solution().reverseVowels2('hello'))
