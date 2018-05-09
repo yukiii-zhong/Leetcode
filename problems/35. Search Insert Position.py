1f93eb75e938c66b4d3eb2705ff85bb0957bfbdc
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-09 22:28:58 +0800

    35 (2

--- problems/35. Search Insert Position.py
+++ problems/35. Search Insert Position.py
@@ -27,4 +27,20 @@ class Solution:
                     j = mid - 1
 
 
-print(Solution().searchInsert([1,3,5,6],0))
+    def searchInsert2(self, nums, target):
+        i = 0
+        j = len(nums)-1
+
+        while j >=i:
+            mid = (i+j)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                i = mid+1
+            else:
+                j = mid-1
+
+        return i
+
+
+print(Solution().searchInsert2([1,3,5,6],8))
