38efb7e2c56660e776cfd73eca35a61484128e9c
yukiii-zhong <30614747+yukiii-zhong@users.noreply.github.com>
2018-05-14 00:41:43 +0800

    347 improvement

--- problems/347. Top K Frequent Elements.py
+++ problems/347. Top K Frequent Elements.py
@@ -35,7 +35,46 @@ class Solution:
         :type k: int
         :rtype: List[int]
         """
-        
+        # mapping: [[num, count], [], []]
+        mapping = []
+        maxcount = 0
+        nums.sort()
 
-test1 = Solution().topKFrequent([3,0,1,0],1)
+        for i in range(len(nums)):
+            if i == 0 or nums[i-1] != nums[i]:
+                mapping.append([nums[i],1])
+            else:
+                mapping[-1][1] +=1
+
+            maxcount = max(maxcount,mapping[-1][1])
+
+        mapping.sort(key=lambda x: x[-1])
+
+        ans = []
+        for i in range(k):
+            ans.append(mapping[-1][0])
+            mapping.pop()
+
+        return ans
+
+    def topKFrequent3(self, nums, k):
+
+        mapping = {}
+
+        for num in nums:
+            if num in mapping:
+                mapping[num] +=1
+            else:
+                mapping[num] = 1
+
+        rank = sorted(mapping.items(), key= lambda item: item[-1])
+
+        ans = []
+        for i in range(k):
+            ans.append(rank[-1][0])
+            rank.pop()
+
+        return ans
+
+test1 = Solution().topKFrequent3([3,3,3,3,0,1,0],2)
 print(test1)
