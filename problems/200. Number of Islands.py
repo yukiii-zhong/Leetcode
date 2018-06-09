61c26f4636c423d59f81fe9ce2d08521b6c9156e
yukizhong <yukiiizhong@gmail.com>
2018-06-09 20:49:19 +0800

    200

--- problems/200. Number of Islands.py
+++ problems/200. Number of Islands.py
@@ -1,17 +1,23 @@
-class Solution:
+class Solution(object):
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid or len(grid[0]) == 0: return 0
 
+        def toWater(grid, i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+                grid[i][j] = 0
+                toWater(grid, i - 1, j)
+                toWater(grid, i + 1, j)
+                toWater(grid, i, j - 1)
+                toWater(grid, i, j + 1)
+
+        if not grid or len(grid[0]) == 0: return 0
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '1' and not (i - 1 >= 0 and grid[i - 1][j] == '1') and not (
-                        j - 1 >= 0 and grid[i][j - 1] == '1'):
+                if grid[i][j] == '1':
                     count += 1
+                    toWater(grid, i, j)
         return count
-
-print(Solution().numIslands([["1","1","1"],["0","1","0"],["1","1","1"]]))
