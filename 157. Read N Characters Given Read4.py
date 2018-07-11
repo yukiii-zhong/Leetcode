ae4c3bf3d79299bd25767cfa968bbe8a3f810730
yukizhong <yukiiizhong@gmail.com>
2018-07-11 16:13:35 +0800

    157

---
+++ 157. Read N Characters Given Read4.py
@@ -0,0 +1,16 @@
+# The read4 API is already defined for you.
+# @param buf, a list of characters
+# @return an integer
+# def read4(buf):
+
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Maximum number of characters to read (int)
+        :rtype: The number of characters read (int)
+        """
+        t = read4(buf)
+        if t>=n: return n
+        elif t<4: return t
+        return 4 + read(buf,n-4)
